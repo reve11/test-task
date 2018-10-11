049e216859846b716871b69d5cdf1de213c4fc32
Roman Shevchenko <rakodil@gmail.com>
2018-10-11 11:21:30 +0200

    implement main logic of tasks

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # test-task
 
-On user sessions:
-I assumed session start will be the first event on product 
-and session end will be the last event. At first I did that 
-the next product event will be the end of session, but looking
-at data it seems unreasonable. Probably it is better to set the
-session end by the type of last action on the product and within 
-external sessions (say, we assuming that end of session is either
-start of next product action or last event time if 5 minutes
-passed since last event on it) but I hadn't time for it.
+On Task#1:
+Session boundaries defined as either previous event is null, meaning session just started, or difference between events
+is greater than some defined value (5 minutes in task#1).
+
+On Task#2.1:
+Median session duration were easy to express using just SQL query.
+
+On Task#2.2:
+I assumed that I should use sessions defined in Task#2 to count time, effectively meaning that users cannot have sessions
+across these defined Task#1 sessions. Time spent counts as sum of differences between events for simplification. I think
+that there can be smarter session boundaries that depend on type of action and/or next user action. 
+
+On user sessions for Task#2.3:
+I assumed session start will be the first event on product and session end will be the last event. At first I did that 
+the next product event will be the end of session, but looking at data it seems unreasonable. Probably it is better to
+set the session end by the type of last action on the product and within external sessions (say, we assuming that end
+of session is either start of next product action or last event time if 5 minutes passed since last event on it).
+
--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -28,26 +28,16 @@ object Main {
     implicit val eventEncoder: Encoder[Event] = Encoders.product[Event]
     import org.apache.spark.sql.expressions.Window
 
+    //Task 1
     val categoryWindow = Window.partitionBy('category).orderBy('eventTime)
-    val globalCategoryWindow = Window.orderBy('category, 'eventTime)
+    val globalWindow = Window.orderBy('category, 'eventTime)
     val sessionWindow = Window.partitionBy('sessionId).orderBy('eventTime)
       .rowsBetween(Window.unboundedPreceding, Window.unboundedFollowing)
     val lastEvent = lag('eventTime, 1).over(categoryWindow).as("lastEvent")
-    val sessionId = sum($"isNewSession").over(globalCategoryWindow).as("sessionId")
+    val sessionId = sum($"isNewSession").over(globalWindow).as("sessionId")
     val sessionStart = min($"eventTime").over(sessionWindow).as("sessionStart")
     val sessionEnd = max($"eventTime").over(sessionWindow).as("sessionEnd")
-
     val sessionBoundaries = $"eventTime".minus($"lastEvent").gt(FIVE_MINUTES).or($"lastEvent".isNull)
-
-    val userCategoryWindow = Window.partitionBy('category, 'userId).orderBy('eventTime)
-    val userTimeSpent = ($"eventTime" - coalesce(lag('eventTime, 1).over(userCategoryWindow), $"eventTime"))
-      .as("spentTime")
-    val ltOne = when($"spentTime" < 60, 1).otherwise(0)
-      .as("less_than_one")
-    val gtFive = when($"spentTime" > 300, 1).otherwise(0)
-      .as("more_than_five")
-    val oneToFive = when($"spentTime" >= 60 and $"spentTime" <= 300, 1).otherwise(0)
-      .as("from_one_to_five")
 
     val parsed = spark.read
       .option("header", "true")
@@ -61,35 +51,59 @@ object Main {
       .select($"*", sessionId)
       .select($"category", $"product", $"userId", $"eventType", $"eventTime", $"sessionId", sessionStart, sessionEnd)
 
-    groupedDf.createOrReplaceTempView("sessioned_events")
 
-    spark.sql("select category, percentile_approx(sessionEnd - sessionStart, 0.5) as median_session_time from sessioned_events group by category")
+    //Task#2 1
+    groupedDf.createOrReplaceTempView("sessioned_events")
+    spark.sql(
+      """select category, percentile_approx(sessionEnd - sessionStart, 0.5) as median_session_time
+        | from sessioned_events
+        |  group by category""".stripMargin)
       .show()
+
+    //Task#2 2
+    val userCategoryWindow = Window.partitionBy('category, 'userId, 'sessionId).orderBy('eventTime)
+    val userTimeSpent = ($"eventTime" - coalesce(lag('eventTime, 1).over(userCategoryWindow), $"eventTime"))
+      .as("spentTime")
+    val ltOne = sum(when($"spentTime" < 60, 1).otherwise(0))
+      .as("less_than_one")
+    val gtFive = sum(when($"spentTime" > 300, 1).otherwise(0))
+      .as("more_than_five")
+    val oneToFive = sum(when($"spentTime" >= 60 and $"spentTime" <= 300, 1).otherwise(0))
+      .as("one_to_five")
+
 
     groupedDf
       .select($"*", userTimeSpent)
       .groupBy($"category", $"userId")
       .agg(sum($"spentTime").as("spentTime"))
-      .select($"*", ltOne, oneToFive, gtFive)
-      .show()
+      .groupBy($"category")
+      .agg(ltOne, oneToFive, gtFive)
+    //      .show()
 
-    val lastEventUserProduct = lag('eventTime, 1).over(Window.partitionBy('userId).orderBy('eventTime))
+    //Task#2 3
+    val userWindow = Window.partitionBy('userId).orderBy('eventTime)
+    val lastEventUserProduct = lag('eventTime, 1).over(userWindow)
       .as("lastUserEvent")
-    val tmpWindow = Window.partitionBy('userId).orderBy('userId, 'eventTime)
-    val isNewUserSession = when(lag('product, 1).over(tmpWindow).notEqual($"product")
+    val isNewUserSession = when(lag('product, 1).over(userWindow).notEqual($"product")
       .or(lastEventUserProduct.isNull), 1)
       .otherwise(0).as("isNewSession")
 
-
     val sessionTimeWindow = Window.partitionBy('sessionId).orderBy('eventTime).rowsBetween(Window.unboundedPreceding, Window.unboundedFollowing)
-    parsed
+    val userSessionStart = first($"eventTime").over(sessionTimeWindow).as("sessionStart")
+    val userSessionEnd = last($"eventTime").over(sessionTimeWindow).as("sessionEnd")
+
+    val userSessionedDF = parsed
       .select($"*", lastEventUserProduct, isNewUserSession)
       .select($"*", sum($"isNewSession").over(Window.orderBy('userId, 'eventTime)).as("sessionId"))
-      .select($"*", first($"eventTime").over(sessionTimeWindow).as("sessionStart"))
-      .select($"*", last($"eventTime").over(sessionTimeWindow).as("sessionEnd"))
-      .select($"sessionId", $"sessionEnd".minus($"sessionStart").as("sessionLength"), $"category", $"product")
+      .select($"*", userSessionStart, userSessionEnd)
+      .select($"sessionId", $"sessionEnd".minus($"sessionStart").as("sessionLength"), $"category", $"product", $"userId")
       .distinct()
-      //      .select(rank().over())
+
+    userSessionedDF
+      .groupBy($"category", $"product")
+      .agg(sum($"sessionLength").as("totalLength"))
+      .select($"*", rank().over(Window.partitionBy('category).orderBy('totalLength desc)).as("rank"))
+      .where($"rank" <= 10)
       .show(100)
 
     spark.close()
